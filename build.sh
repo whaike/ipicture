75a1e84c649d42a3245af21381a00e2c6d79ec21
whaike <huang4573@126.com>
2023-02-10 13:54:59 +0800

    upd: 去掉Linux编译

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ if [ $ENVV = 'arm64' ];then
   go build -o bin/arm64/ipicture  main.go
 elif [ $ENVV = 'windows' ]; then
   CGO_ENABLED=1 GOARCH=amd64 GOOS=windows GOARCH=amd64 CC=/opt/local/bin/x86_64-w64-mingw32-gcc go build -o bin/windows/ipicture.exe -ldflags "-H windowsgui" main.go
+else
+  echo "不支持的编译类型:" $ENVV
 fi
